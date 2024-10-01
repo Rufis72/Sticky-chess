79f0da909fc05a7883ef588744b95aeb4dab9965
Rufis72 <milo.games.silverstein@gmail.com>
2024-10-01 11:18:43 -0700

    Fixed an notation, which was the wrong value

--- Basic_functionallity.py
+++ Basic_functionallity.py
@@ -2,6 +2,8 @@ class IllegalMove(Exception):
     """Illegal move error"""
     pass
 
+# TODO kings cannot move except castling, sometimes piece legal move previews will be completely off, add scaling to legal move preview circles (radius = 1/4 of square edge size
+
 
 class Board:
     """Used to edit the board, get legal moves, and other associated proccessess.
@@ -625,7 +627,7 @@ class Display:
         self.board_notation_white = ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
                           "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
                           "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
-                          "a3", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+                          "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
                           "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
                           "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
                           "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
